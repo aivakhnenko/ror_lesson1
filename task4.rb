4cfcadc7d924985ad2497fddf968af33e85245f5
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2018-05-19 22:19:08 +0300

    Code update after code review

--- task4.rb
+++ task4.rb
@@ -7,7 +7,7 @@ b = gets.to_f
 puts "Укажите коэффициент c:"
 c = gets.to_f
 
-d = b**2 - 4*a*c
+d = b**2 - 4 * a * c
 
 if d < 0
   puts "D = #{d}, корней нет"
@@ -15,7 +15,8 @@ elsif d == 0
   x = -b / (2 * a)
   puts "D = #{d}, x = #{x}"
 else
-  x1 = (-b + Math.sqrt(d)) / (2 * a)
-  x2 = (-b - Math.sqrt(d)) / (2 * a)
+  dsqrt = Math.sqrt(d)
+  x1 = (-b + dsqrt) / (2 * a)
+  x2 = (-b - dsqrt) / (2 * a)
   puts "D = #{d}, x1 = #{x1}, x2 = #{x2}"
 end
